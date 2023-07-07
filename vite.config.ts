6cb44e773f7648e3df02a8b25f47ec2e0196a412
luis <netoardon9@gmail.com>
2023-07-07 00:48:16 -0600

    feat: añadiendo configuracion para PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon'],
+  manifest: {
+    name: 'SimpleStock',
+    short_name: 'SimpleStock',
+    description: 'SimpleStock',
+    icons: [{
+      src: '/icons/0.png',
+      sizes: '192x192',
+      type: 'image/png'
+    },
+    {
+      src: '/icons/512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    }],
+    theme_color: '#01487B',
+    background_color: '#FFFF',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait'
+  },
+  workbox: {
+    globDirectory: "dist",
+    globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,json}"],
+    swDest: "dist/sw.js",
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)], 
 })
